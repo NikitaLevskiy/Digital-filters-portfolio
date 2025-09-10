d778fcd4306e82f863840e1f5e5133fb6ffd56c9
Mykyta Levskyi <nikita.levskiy@gmail.com>
2025-09-10 19:33:47 +0200

    Matlab math model of a FIR BPF

---
+++ FIR/Matlab/BPF_FIR_Window_method.m
@@ -0,0 +1,80 @@
+clear all;
+clc;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Bandpass filter (BPF)
+% Order (N-1), sampling frequency (Fs), cutoff frequencies (Fc1 and Fc2).
+N = 32;
+Fs = 1e6;
+Fc1 = 150e3;
+Fc2 = 300e3;
+
+% Normalised cutoff frequencies of bandpass filter (Wc1 and Wc2).
+Wc1 = 2 * pi * Fc1 / Fs;
+Wc2 = 2 * pi * Fc2 / Fs;
+
+% Filter indexes (n and m).
+n = [0:N-1];
+m = N/2;
+
+% Calculation of coefficients of Hamming window (w).
+for i = 1:N
+
+    w(i) = 0.54 + 0.46 * cos((2 * pi * (n(i) - m)) / (N - 1));
+
+end
+
+% Calculation of impulse response of FIR filter (h).
+for i = 1:N
+
+    if i == N/2 + 1
+        
+        h(i) = (Wc2 - Wc1) / pi;
+    
+    else
+
+        h(i) =  (sin(Wc2 * (n(i) - m)) - sin(Wc1 * (n(i) - m))) ...
+              / (pi * (n(i) - m));
+
+    end  
+
+end
+
+% Applying window to the calculated impulse response (h).
+h = h .* w;
+
+% Calculation of transfer function of the filter (H).
+step = 1;
+f = [0:step:Fs/2];
+
+for i = 1:length(f)
+
+    Hbp(i) = sum(h .* exp(-1j * 2 * pi * f(i) .* (n - m) / Fs));
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Plots
+% Transfer function plots for every filter topology.
+figure;
+
+subplot(2, 2, 1);
+xmin = 0; xmax = N-1; ymin = -25; ymax = 5;
+plot(n, 20*log10(w));
+title('Subplot 1: Window function of a bandpass filter.');
+grid on;
+axis([xmin xmax ymin ymax]);
+
+subplot(2, 2, 2);
+xmin = 0; xmax = N-1; ymin = min(h); ymax = max(h);
+stem(n, h);
+title('Subplot 2: Impulse response of a bandpass filter.');
+grid on;
+axis([xmin xmax ymin ymax]);
+
+subplot(2, 2, 3);
+xmin = 0; xmax = Fs/2; ymin = -100; ymax = 10;
+plot(f, 20*log10(abs(Hbp)));
+title('Subplot 3: Transfer function of a bandpass filter.');
+grid on;
+axis([xmin xmax ymin ymax]);

ed22684f64e809c5dac90cacd08bdc3c44808197
Mykyta Levskyi <nikita.levskiy@gmail.com>
2026-04-10 13:25:57 +0200

    Script update

--- CIC/Matlab/Digital_filter_CIC.m
+++ CIC/Matlab/Digital_filter_CIC.m
@@ -4,10 +4,10 @@ clc;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % CIC filter
 % Sampling frequency (Fs).
-Fs = 1e6;
+Fs_cic = 1e6;
 
 % Rate change (R).
-R = 2;
+R = 32;
 
 % Order (N).
 N = 1;
@@ -16,114 +16,54 @@ N = 1;
 M = 1;
 
 % Input data width (Bin) and Output data width (B).
-Bin = 8;
+Bin = 12;
 B = Bin + N * ceil(log2(R * M))
 
 % Calculation of frequency response of the filter (Hcic).
 step = 1;
-f = [0:step:Fs/2];
+f_cic = [0:step:Fs_cic/R];
 
-for i = 1:length(f)
+for i = 1:length(f_cic)
 
-    Hcic(i) = abs(sin((pi*M*R*f(i)) / Fs) / sin(pi*f(i) / Fs))^N;
+    h_cic(i) = abs(sin((pi*M*R*f_cic(i)) / Fs_cic) / ...
+               sin(pi*f_cic(i) / Fs_cic))^N;
 
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Lowpass filter (LPF)
-% Order (N-1), sampling frequency (Fs), rate change (R), cutoff frequency
-% (Fc).
-N = 64;
-Fs = 1e6;
+% Calculation of frequency response of compensation filter (H).
+% Order (N).
+N = 1;
+step = 1;
+f_lp = [0:step:Fs_cic/(R)];
 
-% Fc should be < Fs / (2*R)
-Fc = 220e3;
+for i = 1:length(f_lp)
 
-% Normalised cutoff frequency of a lowpass filter (Wc).
-Wc = 2 * pi * Fc / Fs;
+    temp = f_lp(i) / (Fs_cic/R);
+    h_lp(i) = abs(sin(pi*temp/R) / sin(pi*M*temp))^N;
+
+end
+
+% Order (N-1).
+N = 32;
+Fc = 12e3;
 
 % Filter indexes (n and m).
 n = [0:N-1];
 m = N/2;
 
-% Calculation of coefficients of Hamming window (w).
-for i = 1:N
-
-    w(i) = 0.54 + 0.46 * cos((2 * pi * (n(i) - m)) / (N - 1));
-
-end
-
-% Calculation of impulse response of FIR filter (h).
-for i = 1:N
-
-    if i == N/2 + 1
-        
-        h(i) = Wc / pi;
-    
-    else
-
-        h(i) =  sin(Wc * (n(i) - m)) / (pi * (n(i) - m));
-
-    end  
-
-end
-
-% Applying window to the calculated impulse response (h).
-h = h .* w;
-
-% Calculation of frequency response of the filter (Hlp).
-step = 1;
-f = [0:step:Fs/2];
-
-for i = 1:length(f)
-
-    Hlp(i) = abs(sum(h .* exp(-1j * 2 * pi * f(i) .* (n - m) / Fs)));
-
-end
-
-% Calculation of frequency response of compensation filter (Hcomp).
-% Order (N).
-N = 1;
-step = 1;
-f = [0:step:Fs/2];
-
-for i = 1:length(f)
-
-    temp = f(i) / (Fs/R);
-    Hcomp(i) = abs(sin(pi*temp/R) / sin(pi*M*temp))^N;
-
-    if i <= Fs / 4
-    
-        Hcomp(i) = Hlp(i) * Hcomp(i);
-    
-    else
-
-        Hcomp(i) = Hlp(i);
-
-    end
-
-
-end
-
-% Order (N-1).
-N = 63;
-
-% Filter indexes (n and m).
-n = [0:N-1];
-m = (N-1)/2;
-
 % Calculation of amplitude vector (Ak).
-temp = floor((Fs / 2) / m);
+temp = floor((Fs_cic / (2*R)) / (m+1));
 
 for i = 1:m
 
     if temp < Fc
     
-        Ak(i) = Hcomp(f(temp));
+        Ak(i) = h_lp(f_lp(temp));
     
     end
 
-    temp = temp + floor((Fs / 2) / m);
+    temp = temp + floor((Fs_cic / (2*R)) / (m+1));
 
 end
 
@@ -141,7 +81,7 @@ for i = 1:length(n)
 
     end
 
-    h(i) = (1/63) * (Ak(1) + 2*temp);
+    h(i) = (1/N) * (Ak(1) + 2*temp);
 
 end
 
@@ -155,11 +95,12 @@ h = h .* w;
 
 % Calculation of frequency response of the filter (H).
 step = 1;
-f = [0:step:Fs/2];
+f = [0:step:Fs_cic/R];
 
 for i = 1:length(f)
 
-    H(i) = abs(sum(h .* exp(-1j * 2 * pi * f(i) .* (n - m) / Fs)));
+    h_lp(i) = abs(sum(h .* exp(-1j * 2 * pi * f(i) .* (n - m) / ...
+              (Fs_cic / (R)))));
 
 end
 
@@ -169,22 +110,22 @@ end
 figure;
 
 subplot(2, 2, 1);
-xmin = 0; xmax = Fs/2; ymin = -100; ymax = 20;
-plot(f, 20*log10(Hcic));
+xmin = 0; xmax = Fs_cic / R; ymin = -60; ymax = 60;
+plot(f_cic, 20*log10(h_cic));
 title('Frequency response of CIC filter.');
 axis([xmin xmax ymin ymax]);
 grid on;
 
 subplot(2, 2, 2);
-xmin = 0; xmax = Fs/2; ymin = -100; ymax = 20;
-plot(f, 20*log10(H));
+xmin = 0; xmax = Fs_cic / (2*R); ymin = -100; ymax = 20;
+plot(f_lp, 20*log10(h_lp));
 title('Frequency response of compemsation filter.');
 axis([xmin xmax ymin ymax]);
 grid on;
 
 subplot(2, 2, 3);
-xmin = 0; xmax = Fs/2; ymin = -100; ymax = 20;
-plot(f, 20*log10(Hcic .* H));
+xmin = 0; xmax = Fs_cic / (2*R); ymin = -100; ymax = 20;
+plot(f_lp, 20*log10(h_cic .* h_lp));
 title('Frequency response of compensated CIC filter.');
 axis([xmin xmax ymin ymax]);
 grid on;
